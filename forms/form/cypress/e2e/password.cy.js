9b33527c070157c559e9f98de3abdb62fb17c947
gisgiselle <brengimolina@gmail.com>
2022-08-29 21:31:31 -0500

    At least 8 chars

--- forms/form/cypress/e2e/password.cy.js
+++ forms/form/cypress/e2e/password.cy.js
@@ -2,19 +2,28 @@
 describe('empty spec', () => {
   beforeEach(() => {
     cy.visit('http://localhost:3000')
-  })
+    cy.get('#nombre').type('giselle')
+    cy.get('#edad').type('15')
+    cy.get('#email').type("giselle@gmail.com")
 
+  })
+  
   it('contains at least one special char',() => {
-    
+   
+
   })
 
   it('does not contain at least one special char',() => {
     
   })
-
-  it('from 8 to 10 characters',() => {
-
+  
+  //fix
+  it('has 8 to 10 characters',() => {
+    cy.get('#pass').type(12345678)
+    cy.get('#pass').invoke('text')
+    .then((text) => {
+      expect(text.length).to.be.at.least(8)
+    })
   })
 
-  it('e')
 })
