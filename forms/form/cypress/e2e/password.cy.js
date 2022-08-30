c6ebc11bf0aba9b16beb5b578d5091bcd1514a31
gisgiselle <brengimolina@gmail.com>
2022-08-29 20:54:58 -0500

    Password specs

---
+++ forms/form/cypress/e2e/password.cy.js
@@ -0,0 +1,19 @@
+describe('empty spec', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000')
+  })
+
+  it('contains at least one special char',() => {
+    
+  })
+
+  it('does not contain at least one special char',() => {
+    
+  })
+
+  it('from 8 to 10 characters',() => {
+
+  })
+
+  it('e')
+})
