8f027bc80f69d12112161fe00c18c85f9a491589
DianaMacias77 <42615630+DianaMacias77@users.noreply.github.com>
2022-09-01 19:48:46 -0500

    prueba

--- forms/form/cypress/e2e/button.cy.js
+++ forms/form/cypress/e2e/button.cy.js
@@ -40,7 +40,8 @@ it('Textfield name is incorrectly filled and click the button', () =>{
 it('Textfield edad is incorrectly filled and click the button', () =>{
   cy.get('#nombre').type('Diana')
   cy.get('input#nombre').type('Diana')
-  cy.get('input#edad').type('veintidos')
+  cy.get('input#edad').type('22')
+  cy.get('input#edad').invoke('text').should('match', /^[0-119]*$/)
   cy.get('input#email').type('diana@gmail.com')
   cy.get('input#pass').type('Dian@123')
   cy.get('input#repass').type('Dian@123')
@@ -51,7 +52,8 @@ it('Textfield email is incorrectly filled and click the button', () =>{
   cy.get('#nombre').type('Diana')
   cy.get('input#nombre').type('Diana')
   cy.get('input#edad').type('22')
-  cy.get('input#email').type('diana@')
+  cy.get('input#email').type('diana@gmail.com')
+  cy.get('input#email').find("[type='email']")
   cy.get('input#pass').type('Dian@123')
   cy.get('input#repass').type('Dian@123')
   cy.get('.Button').click()
@@ -63,6 +65,7 @@ it('Textfield password is incorrectly filled and click the button', () =>{
   cy.get('input#edad').type('22')
   cy.get('input#email').type('diana@gmail.com')
   cy.get('input#pass').type('diana123')
+  //cy.get('input#pass').find"[type='password']"
   cy.get('input#repass').type('dia123')
   cy.get('.Button').click()
 })
@@ -74,6 +77,7 @@ it('Textfield password again is incorrectly filled and click the button', () =>{
   cy.get('input#email').type('diana@gmail.com')
   cy.get('input#pass').type('diana123')
   cy.get('input#repass').type('dian123')
+  //cy.get('input#repass').find"[type='password']"
   cy.get('.Button').click()
 })
 })
